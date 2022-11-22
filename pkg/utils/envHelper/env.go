cdfc36ecbf6e56da66ef6e8eff8d7593ed35141a
Tiger Wang <tigerwang@outlook.com>
2022-11-21 19:58:16 -0500

    wip

---
+++ pkg/utils/envHelper/env.go
@@ -0,0 +1,26 @@
+package envHelper
+
+import "strings"
+
+func ReplaceDefaultENV(key, tz string) string {
+	temp := ""
+	switch key {
+	case "$DefaultPassword":
+		temp = "casaos"
+	case "$DefaultUserName":
+		temp = "admin"
+
+	case "$PUID":
+		temp = "1000"
+	case "$PGID":
+		temp = "1000"
+	case "$TZ":
+		temp = tz
+	}
+	return temp
+}
+
+// replace env default setting
+func ReplaceStringDefaultENV(str string) string {
+	return strings.ReplaceAll(strings.ReplaceAll(str, "$DefaultPassword", ReplaceDefaultENV("$DefaultPassword", "")), "$DefaultUserName", ReplaceDefaultENV("$DefaultUserName", ""))
+}
