5ae3af99c3b3fb1fb3c9bb595ffaa623fc7d26bd
Tiger Wang <tigerwang@outlook.com>
2022-12-21 17:26:45 +0000

    wip
    Signed-off-by: Tiger Wang <tigerwang@outlook.com>

--- service/v2/appstore_test.go
+++ service/v2/appstore_test.go
@@ -3,22 +3,11 @@ package v2
 import (
 	"testing"
 
-	"github.com/compose-spec/compose-go/loader"
-	"github.com/compose-spec/compose-go/types"
 	"gotest.tools/v3/assert"
 )
 
 func TestYAMLUnmarshal(t *testing.T) {
 	for _, v := range catalog {
-		project, err := loader.Load(types.ConfigDetails{
-			ConfigFiles: []types.ConfigFile{
-				{
-					Content: []byte(v),
-				},
-			},
-		})
-
-		assert.NilError(t, err)
-		assert.Equal(t, len(project.Services), 1)
+		assert.Equal(t, len(v.Project.Services), 1)
 	}
 }
