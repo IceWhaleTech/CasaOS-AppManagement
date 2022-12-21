0979c988e4ba447fc4ddb3f65ad840681db0649f
Tiger Wang <tigerwang@outlook.com>
2022-12-21 01:37:44 +0000

    wip
    Signed-off-by: Tiger Wang <tigerwang@outlook.com>

---
+++ route/v2/appstore.go
@@ -0,0 +1,10 @@
+package v2
+
+import (
+	"github.com/IceWhaleTech/CasaOS-AppManagement/codegen"
+	"github.com/labstack/echo/v4"
+)
+
+func (*AppManagement) GetAppInfo(ctx echo.Context, id codegen.StoreAppID) error {
+	return nil
+}
