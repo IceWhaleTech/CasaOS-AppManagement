1346867144249c620100c6388ee9f4074149e661
Tiger Wang <tigerwang@outlook.com>
2022-12-23 04:48:07 +0000

    wip
    Signed-off-by: Tiger Wang <tigerwang@outlook.com>

--- route/v2/appstore.go
+++ route/v2/appstore.go
@@ -11,7 +11,14 @@ import (
 )
 
 func (*AppManagement) AppList(ctx echo.Context) error {
-	return ctx.JSON(http.StatusOK, codegen.ComposeAppStoreListOK{})
+	return ctx.JSON(http.StatusOK, codegen.ComposeAppStoreInfoListsOK{
+		Data: &codegen.ComposeAppStoreInfoLists{
+			// TODO
+			Recommend: &[]codegen.ComposeAppStoreInfo{},
+			List:      &[]codegen.ComposeAppStoreInfo{},
+			Community: &[]codegen.ComposeAppStoreInfo{},
+		},
+	})
 }
 
 func (*AppManagement) AppInfo(ctx echo.Context, id codegen.AppStoreID) error {
@@ -21,18 +28,6 @@ func (*AppManagement) AppInfo(ctx echo.Context, id codegen.AppStoreID) error {
 		return ctx.JSON(http.StatusNotFound, codegen.ResponseNotFound{
 			Message: utils.Ptr("app not found"),
 		})
-	}
-
-	accept := ctx.Request().Header.Get(echo.HeaderAccept)
-	if accept == MIMEApplicationYAML {
-		yaml := composeApp.YAML()
-		if yaml == nil {
-			return ctx.JSON(http.StatusInternalServerError, codegen.ResponseInternalServerError{
-				Message: utils.Ptr("yaml not found"),
-			})
-		}
-
-		return ctx.String(http.StatusOK, *yaml)
 	}
 
 	storeInfo, err := composeApp.StoreInfo()
@@ -56,11 +51,36 @@ func (*AppManagement) AppInfo(ctx echo.Context, id codegen.AppStoreID) error {
 	}
 
 	return ctx.JSON(http.StatusOK, codegen.ComposeAppStoreInfoOK{
-		Data: &codegen.ComposeAppStoreInfoDetails{
+		Data: &codegen.ComposeAppStoreInfo{
 			AppStoreID: storeInfo.AppStoreID,
 			MainApp:    storeInfo.MainApp,
 			Apps:       &apps,
-			Compose:    (*types.Project)(composeApp),
 		},
+	})
+}
+
+func (*AppManagement) AppCompose(ctx echo.Context, id codegen.AppStoreID) error {
+	composeApp := service.MyService.V2AppStore().ComposeApp(id)
+
+	if composeApp == nil {
+		return ctx.JSON(http.StatusNotFound, codegen.ResponseNotFound{
+			Message: utils.Ptr("app not found"),
+		})
+	}
+
+	accept := ctx.Request().Header.Get(echo.HeaderAccept)
+	if accept == MIMEApplicationYAML {
+		yaml := composeApp.YAML()
+		if yaml == nil {
+			return ctx.JSON(http.StatusInternalServerError, codegen.ResponseInternalServerError{
+				Message: utils.Ptr("yaml not found"),
+			})
+		}
+
+		return ctx.String(http.StatusOK, *yaml)
+	}
+
+	return ctx.JSON(http.StatusOK, codegen.ComposeAppOK{
+		Data: (*types.Project)(composeApp),
 	})
 }
