af2135b3e241ce12920785812e9784682adddbdf
Tiger Wang <tigerwang@outlook.com>
2022-12-28 22:51:44 -0500

    wip
    Signed-off-by: Tiger Wang <tigerwang@outlook.com>

--- service/v2/compose_app.go
+++ service/v2/compose_app.go
@@ -1,25 +1,12 @@
 package v2
 
 import (
-	"context"
 	"fmt"
-	"os"
-	"path/filepath"
-	"time"
 
 	"github.com/IceWhaleTech/CasaOS-AppManagement/codegen"
 	"github.com/IceWhaleTech/CasaOS-AppManagement/common"
-	"github.com/IceWhaleTech/CasaOS-AppManagement/pkg/config"
-	"github.com/IceWhaleTech/CasaOS-Common/utils/file"
-	"github.com/IceWhaleTech/CasaOS-Common/utils/logger"
-	"github.com/IceWhaleTech/CasaOS-Common/utils/random"
 	"github.com/compose-spec/compose-go/loader"
 	"github.com/compose-spec/compose-go/types"
-	"github.com/docker/cli/cli/command"
-	"github.com/docker/cli/cli/flags"
-	"github.com/docker/compose/v2/pkg/api"
-	"github.com/docker/compose/v2/pkg/compose"
-	"go.uber.org/zap"
 	"gopkg.in/yaml.v3"
 )
 
@@ -98,70 +85,6 @@ func (a *ComposeApp) Apps() map[string]*App {
 	return apps
 }
 
-func (a *ComposeApp) PrepareInstall() (*ComposeApp, error) {
-	if err := fixProjectName((*codegen.ComposeApp)(a)); err != nil {
-		logger.Error("failed to fix project name", zap.Error(err))
-		return nil, err
-	}
-
-	a.Name = a.Name + "-" + random.RandomString(4, true)
-
-	a.WorkingDir = filepath.Join(config.AppInfo.AppsPath, a.Name)
-
-	if err := file.IsNotExistMkDir(a.WorkingDir); err != nil {
-		logger.Error("failed to create working dir", zap.Error(err), zap.String("path", a.WorkingDir))
-		return nil, err
-	}
-
-	// save to workdir
-	path := filepath.Join(a.WorkingDir, common.ComposeYAMLFileName)
-	if err := os.WriteFile(path, []byte(*a.YAML()), 0o600); err != nil {
-		logger.Error("failed to save compose file", zap.Error(err), zap.String("path", path))
-
-		if err := file.RMDir(a.WorkingDir); err != nil {
-			logger.Error("failed to cleanup working dir after failing to save compose file", zap.Error(err), zap.String("path", a.WorkingDir))
-		}
-		return nil, err
-	}
-
-	return a, nil
-}
-
-func (a *ComposeApp) Install() (*ComposeApp, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Second) // 5 minutes
-
-	go func(ctx context.Context, cancel context.CancelFunc) {
-		defer cancel()
-
-		// pull
-		if err := a.Pull(ctx, cancel); err != nil {
-			logger.Error("failed to pull images for compose app", zap.Error(err))
-			return
-		}
-
-		// prepare install
-		if _, err := a.PrepareInstall(); err != nil {
-			logger.Error("failed to prepare install compose app", zap.Error(err))
-			return
-		}
-
-		// TODO - create
-
-		// TODO - start
-	}(ctx, cancel)
-
-	return a, nil
-}
-
-func (a *ComposeApp) Pull(ctx context.Context, cancel context.CancelFunc) error {
-	service, err := apiService()
-	if err != nil {
-		return err
-	}
-
-	return service.Pull(ctx, (*codegen.ComposeApp)(a), api.PullOptions{})
-}
-
 func NewComposeAppFromYAML(yaml []byte) (*ComposeApp, error) {
 	project, err := loader.Load(
 		types.ConfigDetails{
@@ -204,19 +127,4 @@ func fixProjectName(project *codegen.ComposeApp) error {
 	}
 
 	return nil
-}
-
-func apiService() (api.Service, error) {
-	dockerCli, err := command.NewDockerCli()
-	if err != nil {
-		return nil, err
-	}
-
-	if err := dockerCli.Initialize(&flags.ClientOptions{
-		Common: &flags.CommonOptions{},
-	}); err != nil {
-		return nil, err
-	}
-
-	return compose.NewComposeService(dockerCli), nil
 }
