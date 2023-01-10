a55c2062514cc16f91031311bc69852a8717f202
Tiger Wang <tigerwang@outlook.com>
2023-01-09 23:11:45 -0500

    wip
    Signed-off-by: Tiger Wang <tigerwang@outlook.com>

--- common/constants.go
+++ common/constants.go
@@ -3,7 +3,7 @@ package common
 const (
 	Localhost = "127.0.0.1"
 
-	AppManagementVersion     = "0.4.1"
+	AppManagementVersion     = "0.4.2"
 	AppManagementServiceName = "app-management"
 
 	ContainerLabelV1AppStoreID = "io.casaos.v1.app.store.id"
