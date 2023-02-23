9c082e07248051862cfc2409a7f82e4a76fda312
Tiger Wang <tigerwang@outlook.com>
2023-02-22 22:17:41 -0500

    wip
    Signed-off-by: Tiger Wang <tigerwang@outlook.com>

--- service/appstore_management.go
+++ service/appstore_management.go
@@ -7,11 +7,11 @@ import (
 )
 
 type AppStoreManagement struct {
-	onAppStoreRegister   []func([]string) error
+	onAppStoreRegister   []func(string) error
 	onAppStoreUnregister []func(string) error
 }
 
-func (s *AppStoreManagement) AppStoreList() []codegen.AppStoreMetadata {
+func (a *AppStoreManagement) AppStoreList() []codegen.AppStoreMetadata {
 	return lo.Map(config.ServerInfo.AppStoreList, func(appStoreURL string, id int) codegen.AppStoreMetadata {
 		return codegen.AppStoreMetadata{
 			ID:  &id,
@@ -20,31 +20,39 @@ func (s *AppStoreManagement) AppStoreList() []codegen.AppStoreMetadata {
 	})
 }
 
-func (s *AppStoreManagement) OnAppStoreRegister(fn func([]string) error) {
-	s.onAppStoreRegister = append(s.onAppStoreRegister, fn)
+func (a *AppStoreManagement) OnAppStoreRegister(fn func(string) error) {
+	a.onAppStoreRegister = append(a.onAppStoreRegister, fn)
 }
 
-func (s *AppStoreManagement) OnAppStoreUnregister(fn func(string) error) {
-	s.onAppStoreUnregister = append(s.onAppStoreUnregister, fn)
+func (a *AppStoreManagement) OnAppStoreUnregister(fn func(string) error) {
+	a.onAppStoreUnregister = append(a.onAppStoreUnregister, fn)
 }
 
-func (s *AppStoreManagement) RegisterAppStore(appstoreURL string) error {
+func (a *AppStoreManagement) RegisterAppStore(appstoreURL string) error {
 	config.ServerInfo.AppStoreList = append(config.ServerInfo.AppStoreList, appstoreURL)
 
-	for _, fn := range s.onAppStoreRegister {
-		if err := fn(config.ServerInfo.AppStoreList); err != nil {
+	if err := config.SaveSetup(); err != nil {
+		return err
+	}
+
+	for _, fn := range a.onAppStoreRegister {
+		if err := fn(appstoreURL); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func (s *AppStoreManagement) UnregisterAppStore(appStoreID uint) error {
+func (a *AppStoreManagement) UnregisterAppStore(appStoreID uint) error {
 	appStoreURL := config.ServerInfo.AppStoreList[appStoreID]
 
 	config.ServerInfo.AppStoreList = append(config.ServerInfo.AppStoreList[:appStoreID], config.ServerInfo.AppStoreList[appStoreID+1:]...)
 
-	for _, fn := range s.onAppStoreUnregister {
+	if err := config.SaveSetup(); err != nil {
+		return err
+	}
+
+	for _, fn := range a.onAppStoreUnregister {
 		if err := fn(appStoreURL); err != nil {
 			return err
 		}
---
+++ service/appstore_management_test.go
@@ -0,0 +1,54 @@
+package service
+
+import (
+	"os"
+	"testing"
+
+	"github.com/IceWhaleTech/CasaOS-AppManagement/pkg/config"
+	"gotest.tools/v3/assert"
+)
+
+func TestAppStoreList(t *testing.T) {
+	// defer goleak.VerifyNone(t)
+
+	file, err := os.CreateTemp("", "app-management.conf")
+	assert.NilError(t, err)
+
+	defer os.Remove(file.Name())
+
+	config.InitSetup(file.Name())
+
+	appStoreManagement := NewAppStoreManagement()
+
+	appStoreList := appStoreManagement.AppStoreList()
+	assert.Equal(t, len(appStoreList), 1)
+
+	registeredAppStoreList := []string{}
+	appStoreManagement.OnAppStoreRegister(func(appStoreURL string) error {
+		registeredAppStoreList = append(registeredAppStoreList, appStoreURL)
+		return nil
+	})
+
+	unregisteredAppStoreList := []string{}
+	appStoreManagement.OnAppStoreUnregister(func(appStoreURL string) error {
+		unregisteredAppStoreList = append(unregisteredAppStoreList, appStoreURL)
+		return nil
+	})
+
+	expectAppStoreURL := "https://appstore.example.com"
+	err = appStoreManagement.RegisterAppStore(expectAppStoreURL)
+	assert.NilError(t, err)
+	assert.Assert(t, len(registeredAppStoreList) == 1)
+
+	appStoreList = appStoreManagement.AppStoreList()
+	assert.Equal(t, len(appStoreList), 2)
+
+	actualAppStoreURL := *appStoreList[1].URL
+	assert.Equal(t, actualAppStoreURL, expectAppStoreURL)
+
+	err = appStoreManagement.UnregisterAppStore(1)
+	assert.NilError(t, err)
+	assert.Assert(t, len(unregisteredAppStoreList) == 1)
+
+	assert.DeepEqual(t, registeredAppStoreList, unregisteredAppStoreList)
+}
