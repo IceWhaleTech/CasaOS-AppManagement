a1eb726bc202a06ed8d48c89137f4964df385e9d
Tiger Wang <tigerwang@outlook.com>
2023-02-22 23:03:54 -0500

    wip
    Signed-off-by: Tiger Wang <tigerwang@outlook.com>

--- route/v2/appstore.go
+++ route/v2/appstore.go
@@ -3,6 +3,7 @@ package v2
 import (
 	"fmt"
 	"net/http"
+	"net/url"
 
 	"github.com/IceWhaleTech/CasaOS-AppManagement/codegen"
 	"github.com/IceWhaleTech/CasaOS-AppManagement/common"
@@ -24,11 +25,38 @@ func (a *AppManagement) AppStoreList(ctx echo.Context) error {
 }
 
 func (a *AppManagement) RegisterAppStore(ctx echo.Context, params codegen.RegisterAppStoreParams) error {
-	panic("not implemented")
+	appstoreURL, err := url.Parse(*params.Url)
+	if err != nil {
+		message := err.Error()
+		return ctx.JSON(http.StatusBadRequest, codegen.ResponseBadRequest{Message: &message})
+	}
+
+	if err := service.MyService.AppStoreManagement().RegisterAppStore(appstoreURL.String()); err != nil {
+		message := err.Error()
+		return ctx.JSON(http.StatusInternalServerError, codegen.ResponseInternalServerError{Message: &message})
+	}
+
+	return ctx.JSON(http.StatusOK, codegen.AppStoreRegisterOK{
+		Message: utils.Ptr("new app store is registered, and will be pulled asynchronously."),
+	})
 }
 
 func (a *AppManagement) UnregisterAppStore(ctx echo.Context, id codegen.AppStoreID) error {
-	panic("not implemented")
+	appStoreList := service.MyService.AppStoreManagement().AppStoreList()
+
+	if id < 0 || id >= len(appStoreList) {
+		message := fmt.Sprintf("app store id %d is not found", id)
+		return ctx.JSON(http.StatusNotFound, codegen.ResponseNotFound{Message: &message})
+	}
+
+	if err := service.MyService.AppStoreManagement().UnregisterAppStore(uint(id)); err != nil {
+		message := err.Error()
+		return ctx.JSON(http.StatusInternalServerError, codegen.ResponseInternalServerError{Message: &message})
+	}
+
+	return ctx.JSON(http.StatusOK, codegen.AppStoreUnregisterOK{
+		Message: utils.Ptr("app store is unregistered."),
+	})
 }
 
 func (a *AppManagement) ComposeAppStoreInfoList(ctx echo.Context) error {
