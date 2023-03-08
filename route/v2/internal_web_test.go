10c084ef5fc1e2f6cbbbea8c9cf30d70e453aa1a
Tiger Wang <tigerwang@outlook.com>
2023-03-08 18:55:51 -0500

    wip

--- route/v2/internal_web_test.go
+++ route/v2/internal_web_test.go
@@ -61,7 +61,7 @@ func TestWebAppGridItemAdapter(t *testing.T) {
 
 	assert.Equal(t, *gridItem.Icon, (*storeInfo.Apps)[*storeInfo.MainApp].Icon)
 	assert.Equal(t, *gridItem.Image, composeApp.Services[0].Image)
-	assert.Equal(t, *gridItem.Hostname, *(*storeInfo.Apps)[*storeInfo.MainApp].Container.Hostname)
+	assert.Equal(t, gridItem.Hostname, (*storeInfo.Apps)[*storeInfo.MainApp].Container.Hostname)
 	assert.Equal(t, *gridItem.Port, (*storeInfo.Apps)[*storeInfo.MainApp].Container.PortMap)
 	assert.Equal(t, *gridItem.Index, (*storeInfo.Apps)[*storeInfo.MainApp].Container.Index)
 	assert.Equal(t, *gridItem.Status, "running")
