a10ce0829e059dcf67ed47ee4dd8cf93091f7d90
CorrectRoadH <a778917369@gmail.com>
2023-07-25 10:20:09 +0800

    fix/ fix 1774 only have $ when save in setting panel

--- service/utils.go
+++ service/utils.go
@@ -3,6 +3,9 @@ package service
 import (
 	"regexp"
 	"strings"
+
+	"github.com/IceWhaleTech/CasaOS-Common/utils"
+	"gopkg.in/yaml.v3"
 )
 
 var nonAlphaNumeric = regexp.MustCompile(`[^a-z0-9]+`)
@@ -25,4 +28,17 @@ func Standardize(text string) string {
 	result = strings.Trim(result, "-")
 
 	return result
+}
+
+func GenerateYAMLFromComposeApp(compose ComposeApp) ([]byte, error) {
+	// to duplicate Specify Chars
+	for _, service := range compose.Services {
+		// it should duplicate all values that contains $. But for now, we only duplicate the env values
+		for key, value := range service.Environment {
+			if strings.ContainsAny(*value, "$") {
+				service.Environment[key] = utils.Ptr(strings.Replace(*value, "$", "$$", -1))
+			}
+		}
+	}
+	return yaml.Marshal(compose)
 }
