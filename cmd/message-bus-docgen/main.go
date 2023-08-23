0dde0bf18d6b6b0562789a1c39d63c871f51ca4d
Tiger Wang <tigerwang@outlook.com>
2023-08-23 18:05:26 +0800

    wip

---
+++ cmd/message-bus-docgen/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/IceWhaleTech/CasaOS-AppManagement/codegen/message_bus"
+	"github.com/IceWhaleTech/CasaOS-AppManagement/common"
+	"github.com/IceWhaleTech/CasaOS-Common/external"
+	"github.com/samber/lo"
+)
+
+func main() {
+	eventTypes := lo.Map(common.EventTypes, func(item message_bus.EventType, index int) external.EventType {
+		return external.EventType{
+			Name:     item.Name,
+			SourceID: item.SourceID,
+			PropertyTypeList: lo.Map(
+				item.PropertyTypeList, func(item message_bus.PropertyType, index int) external.PropertyType {
+					return external.PropertyType{
+						Name:        item.Name,
+						Description: item.Description,
+						Example:     item.Example,
+					}
+				},
+			),
+		}
+	})
+
+	external.PrintEventTypesAsMarkdown(common.AppManagementServiceName, common.AppManagementVersion, eventTypes)
+}
