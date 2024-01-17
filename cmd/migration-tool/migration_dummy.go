15fefa49904183dfa1da83f6cfc86fb6cf3cd7f5
CorrectRoadH <a778917369@gmail.com>
2024-01-17 10:28:55 +0800

    feat: add migration for new appstore zip

--- cmd/migration-tool/migration_dummy.go
+++ cmd/migration-tool/migration_dummy.go
@@ -1,12 +1,36 @@
 package main
 
 import (
+	"io"
+	"os"
+	"strings"
+
+	"github.com/IceWhaleTech/CasaOS-AppManagement/pkg/config"
 	interfaces "github.com/IceWhaleTech/CasaOS-Common"
+)
+
+const (
+	oldAppStoreURL = "https://github.com/IceWhaleTech/_appstore/archive/refs/heads/main.zip"
+	newAppStoreURL = "https://casaos-appstore.github.io/casaos-appstore/linux-all-appstore.zip"
 )
 
 type migrationTool struct{}
 
 func (u *migrationTool) IsMigrationNeeded() (bool, error) {
+	// read string from AppManagementConfigFilePath
+	file, err := os.Open(config.AppManagementConfigFilePath)
+	if err != nil {
+		return false, err
+	}
+	defer file.Close()
+	content, err := io.ReadAll(file)
+	if err != nil {
+		return false, err
+	}
+	if strings.Contains(string(content), oldAppStoreURL) {
+		return true, nil
+	}
+
 	return false, nil
 }
 
@@ -15,6 +39,22 @@ func (u *migrationTool) PreMigrate() error {
 }
 
 func (u *migrationTool) Migrate() error {
+	// replace string in AppManagementConfigFilePath
+	// replace https://github.com/IceWhaleTech/_appstore/archive/refs/heads/main.zip to https://casaos-appstore.github.io/casaos-appstore/linux-all-appstore.zip
+	file, err := os.OpenFile(config.AppManagementConfigFilePath, os.O_RDWR, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	content, err := io.ReadAll(file)
+	if err != nil {
+		return err
+	}
+	newContent := strings.Replace(string(content), oldAppStoreURL, newAppStoreURL, -1)
+	_, err = file.WriteAt([]byte(newContent), 0)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
