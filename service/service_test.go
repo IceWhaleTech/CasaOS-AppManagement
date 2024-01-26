68d8679eaa6db4cbe83aaa6462bca42b6476676e
CorrectRoadH <a778917369@gmail.com>
2024-01-26 11:32:55 +0800

    test: fix test

--- service/service_test.go
+++ service/service_test.go
@@ -3,4 +3,6 @@ package service_test
 const (
 	topFunc1  = "go.opencensus.io/stats/view.(*worker).start"
 	pollFunc1 = "internal/poll.runtime_pollWait"
+
+	httpFunc1 = "net/http.(*persistConn).writeLoop"
 )
