7cc84898d84d84a3969d5bc177bf38a236371cfb
Ns2Kracy <ns2kracy@gmail.com>
2024-05-30 07:34:53 +0000

    basic v1 api doc

--- route/v1.go
+++ route/v1.go
@@ -33,26 +33,26 @@ func InitV1Router() *gin.Engine {
 		v1ContainerGroup.Use()
 		{
 
-			v1ContainerGroup.GET("", v1.MyAppList) ///my/list
+			// v1ContainerGroup.GET("", v1.MyAppList) ///my/list
 			v1ContainerGroup.GET("/usage", v1.AppUsageList)
-			v1ContainerGroup.GET("/:id", v1.ContainerUpdateInfo)      ///update/:id/info
-			v1ContainerGroup.GET("/:id/compose", v1.ToComposeYAML)    // /app/setting/:id
-			v1ContainerGroup.GET("/:id/logs", v1.ContainerLog)        // /app/logs/:id
+			// v1ContainerGroup.GET("/:id", v1.ContainerUpdateInfo)   ///update/:id/info
+			v1ContainerGroup.GET("/:id/compose", v1.ToComposeYAML) // /app/setting/:id
+			// v1ContainerGroup.GET("/:id/logs", v1.ContainerLog)        // /app/logs/:id
 			v1ContainerGroup.GET("/networks", v1.GetDockerNetworks)   // /app/install/config
 			v1ContainerGroup.PUT("/archive/:id", v1.ArchiveContainer) // /container/archive/:id
 
-			v1ContainerGroup.GET("/:id/state", v1.GetContainerState) // app/state/:id ?state=install_progress
+			// v1ContainerGroup.GET("/:id/state", v1.GetContainerState) // app/state/:id ?state=install_progress
 			// there are problems, temporarily do not deal with
 			v1ContainerGroup.GET("/:id/terminal", v1.DockerTerminal) // app/terminal/:id
-			v1ContainerGroup.POST("", v1.InstallApp)                 // app/install
+			// v1ContainerGroup.POST("", v1.InstallApp)                 // app/install
 
 			v1ContainerGroup.PUT("/:id", v1.UpdateSetting) ///update/:id/setting
 
-			v1ContainerGroup.PUT("/:id/state", v1.ChangAppState) // /app/state/:id
-			v1ContainerGroup.DELETE("/:id", v1.UninstallApp)     // app/uninstall/:id
+			// v1ContainerGroup.PUT("/:id/state", v1.ChangAppState) // /app/state/:id
+			v1ContainerGroup.DELETE("/:id", v1.UninstallApp) // app/uninstall/:id
 
-			v1ContainerGroup.GET("/info", v1.GetDockerDaemonConfiguration)
-			v1ContainerGroup.PUT("/info", v1.PutDockerDaemonConfiguration)
+			// v1ContainerGroup.GET("/info", v1.GetDockerDaemonConfiguration)
+			// v1ContainerGroup.PUT("/info", v1.PutDockerDaemonConfiguration)
 
 		}
 	}
