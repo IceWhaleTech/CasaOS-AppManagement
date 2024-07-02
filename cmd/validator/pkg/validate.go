b0b2c3b149b95bda2ac6fe0dad38a78b9df2b449
CorrectRoad <a778917369@gmail.com>
2024-07-02 14:51:56 +0800

    feat: add more test case to validator (#201)

---
+++ cmd/validator/pkg/validate.go
@@ -0,0 +1,31 @@
+package pkg
+
+import (
+	"github.com/IceWhaleTech/CasaOS-AppManagement/codegen"
+	"github.com/IceWhaleTech/CasaOS-AppManagement/common"
+	"github.com/IceWhaleTech/CasaOS-AppManagement/service"
+	"github.com/compose-spec/compose-go/loader"
+)
+
+func VaildDockerCompose(yaml []byte) (err error) {
+	err = nil
+	// recover
+	defer func() {
+		if r := recover(); r != nil {
+			err = r.(error)
+		}
+	}()
+	docker, err := service.NewComposeAppFromYAML(yaml, false, false)
+
+	ex, ok := docker.Extensions[common.ComposeExtensionNameXCasaOS]
+	if !ok {
+		return service.ErrComposeExtensionNameXCasaOSNotFound
+	}
+
+	var storeInfo codegen.ComposeAppStoreInfo
+	if err = loader.Transform(ex, &storeInfo); err != nil {
+		return
+	}
+
+	return
+}
