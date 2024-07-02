9702402f4ade53c6d60d484168bfd058d641aa0c
CorrectRoadH <a778917369@gmail.com>
2024-07-02 06:43:29 +0000

    wip

--- cmd/validator/pkg/validate.go
+++ cmd/validator/pkg/validate.go
@@ -1,9 +1,24 @@
 package pkg
 
-import "github.com/IceWhaleTech/CasaOS-AppManagement/service"
+import (
+	"github.com/IceWhaleTech/CasaOS-AppManagement/codegen"
+	"github.com/IceWhaleTech/CasaOS-AppManagement/common"
+	"github.com/IceWhaleTech/CasaOS-AppManagement/service"
+	"github.com/compose-spec/compose-go/loader"
+)
 
 func VaildDockerCompose(yaml []byte) error {
-	_, err := service.NewComposeAppFromYAML(yaml, false, false)
+	docker, err := service.NewComposeAppFromYAML(yaml, false, false)
+
+	ex, ok := docker.Extensions[common.ComposeExtensionNameXCasaOS]
+	if !ok {
+		return service.ErrComposeExtensionNameXCasaOSNotFound
+	}
+
+	var storeInfo codegen.ComposeAppStoreInfo
+	if err := loader.Transform(ex, &storeInfo); err != nil {
+		return err
+	}
 
 	return err
 }
